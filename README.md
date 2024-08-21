fe75fb38a2fe5b4b342fe227ec3bff4a6e40dc2d
Jose Falcon <96765773+josefalconGH@users.noreply.github.com>
2024-08-21 15:59:30 -0500

    final commit

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
 # pwa-text-editor
 
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-
 ## Description
+
+`pwa-text-editor` is an offline text editor. It is a Progressive Web Application (PWA) that can be installed on your device and used offline. It is built using React and TypeScript.
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
 ## Features
 
--
--
--
--
--
+- Offline text editing
+- Save text to local storage
+- Load text from local storage
+- Installable on your device
 
 ## Learning Outcomes
 
--
--
--
--
+- Building a Progressive Web Application (PWA)
+- Using React and TypeScript
+- Using local storage in a PWA
 
 1. Clone the repository
 
@@ -29,4 +29,10 @@
 
    ```bash
    npm install
+   ```
+
+3. Start the development server
+
+   ```bash
+   npm start
    ```
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,73 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { InjectManifest } = require("workbox-webpack-plugin");
+
+const path = require("path");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
+      new WebpackPwaManifest({
+        name: "JATE",
+        short_name: "JATE",
+        description: "Take notes and stay organized with JATE.",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        crossorigin: "anonymous",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 256],
+            purpose: "maskable",
+          },
+        ],
+        inject: true,
+        fingerprints: false,
+        publicPath: "./",
+      }),
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Webpack Plugin",
+      }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "sw.js",
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
       ],
     },
   };
